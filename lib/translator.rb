28284bd17933b5cb43a0c6186a0466a420d84843
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-20 02:05:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
+require "yaml"
+
 # require modules here
 
-def load_library
-  # code goes here
+def load_library 
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+  emoticons
+end #outputs hash collection {:meaning => "english", "japanese"}
+
+def get_japanese_emoticon(string)
+    #code to take input string, scan hash Load_library to locate if input matches any "english" values, if so it returns the "japanese" value of the same key
+  load_library.each do |meaning| #throughout the load_library hash, for each entry
+    if load_library[:meaning][0] == string #if the first value ("english") is a match
+      load_library[:meaning][1] #return "japanese"
+    end
+  end
+
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+def get_english_meaning(string)
+    #code to take input string, scan hash load_library to locate if input matches any "japanese" values, if so returns "english" value of same key
+  load_library.each do |meaning| #throughout the load_library hash, for each entry
+    if load_library[:meaning][1] == string #if the second value ("japanese") is a match
+      load_library[:meaning][0] #return "english"
+    end
 end
