ac056e50d29a44b6a512133cf12551eb7fc66ec9
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-20 02:21:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,6 @@ end #outputs hash collection {:meaning => "english", "japanese"}
 
 def get_japanese_emoticon(string)
   pp load_library
-    #code to take input string, scan hash Load_library to locate if input matches any "english" values, if so it returns the "japanese" value of the same key
   load_library.each do |meaning, | #throughout the load_library hash, for each entry
     if load_library[:meaning][0] == string #if the first value ("english") is a match
       load_library[:meaning][1] #return "japanese"
@@ -19,7 +18,6 @@ def get_japanese_emoticon(string)
 end
 
 def get_english_meaning(string)
-    #code to take input string, scan hash load_library to locate if input matches any "japanese" values, if so returns "english" value of same key
   load_library.each do |meaning| #throughout the load_library hash, for each entry
     if load_library[:meaning][1] == string #if the second value ("japanese") is a match
       load_library[:meaning][0] #return "english"
